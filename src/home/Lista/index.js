958e05b24a6d8999f22a105907f223cb4e56a674
felipeslopes <felipesl.lopes@hotmail.com>
2023-09-20 14:57:07 -0300

    alinhando itens da flatlist

--- src/home/Lista/index.js
+++ src/home/Lista/index.js
@@ -16,8 +16,8 @@ export default function Lista({ data }) {
     return (
         <Container activeOpacity={0.7} onPress={() => openModal(true)}
             style={{ width: itemWidth, height: itemWidth }}>
-                
-            <ImagePerson source={{ uri: data.image }} resizeMode="contain" />
+
+            <ImagePerson source={{ uri: data.image }} />
             <Name>{data.name}</Name>
 
             <Modal visible={modalVisible} transparent={true} animationType="slide">
@@ -32,12 +32,12 @@ export default function Lista({ data }) {
 }
 
 const Container = styled.TouchableOpacity`
-flex: 1;
-margin: 2px;
+margin: 2px 0;
+align-items: center;
 `;
 
 const ImagePerson = styled.Image`
-width: 100%;
+width: 98%;
 height: 100%;
 border-radius: 15px;
 `;
@@ -46,10 +46,10 @@ const Name = styled.Text`
 background-color: rgba(0,0,0,0.8);
 position: absolute;
 bottom: 0;
-width: 100%;
+width: 98%;
 text-align: center;
 color: #FFF;
 font-size: 17px;
-border-bottom-left-radius: 10px;
-border-bottom-right-radius: 10px;
+border-bottom-left-radius: 15px;
+border-bottom-right-radius: 15px;
 `;
